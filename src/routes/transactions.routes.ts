0994de43a6d15ad3d8d3921b02d59950f279361b
Bruno Martins <brunorodrigues929@gmail.com>
2020-06-26 18:44:46 -0300

    Progress 26/06-18:45

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,45 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
-
+const upload = multer(uploadConfig);
 transactionsRouter.get('/', async (request, response) => {
   // TODO
+  return response.json({ ok: true });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  // const { title, value, type, category } = request.body;
+  // const transaction = await
+  return response.json({ ok: true });
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
   // TODO
+  return response.json({ ok: true });
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('table'),
+  async (request, response) => {
+    const updateTable = new ImportTransactionsService();
+    // const categories =
+    await updateTable.execute({
+      tableFileName: request.file.filename,
+    });
+    // console.log(categories);
+    // Service pra tratar o arq
+    // const
+    // TODO
+    return response.json({ ok: true });
+  },
+);
 
 export default transactionsRouter;
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import ParseCSV from 'csv-parse';
 import Transaction from '../models/Transaction';
+import uploadConfig from '../config/upload';
+
+interface Request {
+  tableFileName: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ tableFileName }: Request): Promise<void> {
+    // async execute({ tableFileName }: Request): Promise<Transaction[]> {
+    const pathTable = path.join(uploadConfig.directory, tableFileName);
+
+    const file = await fs.promises.readFile(pathTable, 'utf-8');
+    console.log(file);
+    // falta converter para JSON
   }
 }
 
